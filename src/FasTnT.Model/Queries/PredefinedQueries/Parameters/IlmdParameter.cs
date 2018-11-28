c8f89b17b76a37f35d68aa339cb84a427cf13948
Louis-Axel Ambroise <ambroise.la@gmail.com>
2018-11-28 16:03:14 +0100

    Update ILMD parameter handling
    - Parse Namespace and Property
    - Implement PgSQL query for ILMD parameters

--- src/FasTnT.Model/Queries/PredefinedQueries/Parameters/IlmdParameter.cs
+++ src/FasTnT.Model/Queries/PredefinedQueries/Parameters/IlmdParameter.cs
@@ -1,7 +1,11 @@
-﻿namespace FasTnT.Model.Queries.PredefinedQueries.Parameters
+﻿using System.Linq;
+
+namespace FasTnT.Model.Queries.PredefinedQueries.Parameters
 {
     public class IlmdParameter : SimpleEventQueryParameter
     {
+        public string Namespace => Name.Split('_').Single(x => x.Contains('#')).Split('#', 2)[0];
+        public string Property => Name.Split('_').Single(x => x.Contains('#')).Split('#', 2)[1];
         public bool IsInner { get; set; }
     }
 }
