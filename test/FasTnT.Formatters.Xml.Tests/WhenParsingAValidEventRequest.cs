4087b78614c02e76e5d860597593a0f1d2f70a51
Louis-Axel Ambroise <ambroise.la@gmail.com>
2018-12-03 13:01:28 +0100

    Update XML Parsing unit tests

--- test/FasTnT.Formatters.Xml.Tests/WhenParsingAValidEventRequest.cs
+++ test/FasTnT.Formatters.Xml.Tests/WhenParsingAValidEventRequest.cs
@@ -52,5 +52,61 @@ namespace FasTnT.Formatters.Xml.Tests
         {
             Assert.AreEqual(1, ParsedEvents.EventList.Count());
         }
+
+        [Assert]
+        public void TheEventShouldBeAnObjectEvent()
+        {
+            var parsedEvent = ParsedEvents.EventList.First();
+            Assert.AreEqual(EventType.Object, parsedEvent.Type);
+        }
+
+        [Assert]
+        public void TheEventShouldHaveAddAction()
+        {
+            var parsedEvent = ParsedEvents.EventList.First();
+            Assert.AreEqual(EventAction.Add, parsedEvent.Action);
+        }
+
+        [Assert]
+        public void TheEventEPCListShouldContainThreeValues()
+        {
+            var parsedEvent = ParsedEvents.EventList.First();
+            Assert.AreEqual(3, parsedEvent.Epcs.Count);
+        }
+
+        [Assert]
+        public void TheEventEPCListShouldAllBeListType()
+        {
+            var parsedEvent = ParsedEvents.EventList.First();
+            Assert.IsTrue(parsedEvent.Epcs.All(x => x.Type == EpcType.List));
+        }
+
+        [Assert]
+        public void TheEventBusinessStepShouldBePacking()
+        {
+            var parsedEvent = ParsedEvents.EventList.First();
+            Assert.AreEqual("urn:epcglobal:bizstep:packing", parsedEvent.BusinessStep);
+        }
+
+        [Assert]
+        public void TheEventDispositionShouldBeLoading()
+        {
+            var parsedEvent = ParsedEvents.EventList.First();
+            Assert.AreEqual("urn:epcglobal:cbv:disp:loading", parsedEvent.Disposition);
+        }
+
+        [Assert]
+        public void TheEventReadPointShouldBeCorrectlyParsed()
+        {
+            var parsedEvent = ParsedEvents.EventList.First();
+            Assert.AreEqual("urn:fastnt:demo:readpoint:0037000.00729.210,432", parsedEvent.ReadPoint);
+        }
+
+        [Assert]
+        public void TheEventBusinessLocationShouldBeCorrectlyParsed()
+        {
+            var parsedEvent = ParsedEvents.EventList.First();
+            Assert.AreEqual("urn:fastnt:demo:bizloc:0037000.00729.210", parsedEvent.BusinessLocation);
+        }
     }
 }
