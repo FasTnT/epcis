42b59596821513f80d8b6a0f57854f5bc7ef4200
Louis-Axel Ambroise <ambroise.la@gmail.com>
2018-12-07 15:48:53 +0100

    Create subscription model

--- src/FasTnT.Model/Subscriptions/Subscription.cs
+++ src/FasTnT.Model/Subscriptions/Subscription.cs
@@ -1,8 +1,14 @@
-﻿namespace FasTnT.Model.Subscriptions
+﻿using FasTnT.Model.Queries.PredefinedQueries.Parameters;
+using System.Collections.Generic;
+
+namespace FasTnT.Model.Subscriptions
 {
     public class Subscription
     {
+        public string QueryName { get; set; }
+        public IList<SimpleEventQueryParameter> Params { get; set; }
         public string Destination { get; set; }
-        public bool ReportIfEmpty { get; set; }
+        public SubscriptionControls Controls { get; set; }
+        public string SubscriptionId { get; set; }
     }
 }
---
+++ src/FasTnT.Model/Subscriptions/SubscriptionControls.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FasTnT.Model.Subscriptions
+{
+    public class SubscriptionControls
+    {
+        public QuerySchedule Schedule { get; set; }
+        public string Trigger { get; set; }
+        public DateTime InitialRecordTime { get; set; }
+        public bool ReportIfEmpty { get; set; }
+    }
+}
