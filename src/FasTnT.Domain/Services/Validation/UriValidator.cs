89c69b3257d5a212b896014c29e51c1b873f5fa7
Louis-Axel Ambroise <ambroise.la@gmail.com>
2019-01-31 09:35:23 +0100

    ValidateEPC uris in capture request

---
+++ src/FasTnT.Domain/Services/Validation/UriValidator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace FasTnT.Domain.Services
+{
+    public static class UriValidator
+    {
+        private static readonly Regex UriRegex = new Regex(@"^\w+:(\/?\/?)[^\s]+?$", RegexOptions.IgnoreCase | RegexOptions.Compiled);
+        private static readonly Regex HttpRegex = new Regex(@"^(https?):(\/\/)[^\s]+?$", RegexOptions.IgnoreCase | RegexOptions.Compiled);
+
+        public static void Validate(string uri, bool httpOnly = false)
+        {
+            if (!(httpOnly ? HttpRegex : UriRegex).IsMatch(uri)) throw new Exception($"URI not valid: '{uri}'");
+        }
+    }
+}
