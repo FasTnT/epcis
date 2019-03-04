f891ddd10b58a2e1aaf367015e1cdd27668c7498
Louis-Axel Ambroise <ambroise.la@gmail.com>
2019-03-04 08:34:14 +0100

    FIx migration endpoints

--- src/FasTnT.Host/Middleware/Epcis/EpcisMiddlewareExtensions.cs
+++ src/FasTnT.Host/Middleware/Epcis/EpcisMiddlewareExtensions.cs
@@ -7,6 +7,6 @@ namespace FasTnT.Host
         public static IApplicationBuilder UseEpcisCaptureEndpoint(this IApplicationBuilder app, string path) => app.UseMiddleware<EpcisCaptureMiddleware>(path);
         public static IApplicationBuilder UseEpcisQueryEndpoint(this IApplicationBuilder app, string path) => app.UseMiddleware<EpcisQueryMiddleware>(path);
         public static IApplicationBuilder UseEpcisSubscriptionTrigger(this IApplicationBuilder app, string path) => app.UseMiddleware<EpcisSubscriptionTriggerMiddleware>(path);
-        public static IApplicationBuilder UseEpcisMigrationEndpoint(this IApplicationBuilder app, string path, bool isEnabled = true) => isEnabled ? app.UseMiddleware<EpcisMigrationMiddleware>(path) : app;
+        public static IApplicationBuilder UseEpcisMigrationEndpoint(this IApplicationBuilder app, string path) => app.UseMiddleware<EpcisMigrationMiddleware>(path);
     }
 }
