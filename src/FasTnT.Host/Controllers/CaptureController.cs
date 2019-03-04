c060b3d47e8a694d22a132cf776e6c6aa8bde9c9
Louis-Axel Ambroise <ambroise.la@gmail.com>
2019-03-04 08:18:41 +0100

    Add EPCIS 2.0 controllers

---
+++ src/FasTnT.Host/Controllers/CaptureController.cs
@@ -0,0 +1,21 @@
+﻿using FasTnT.Domain.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace FasTnT.Host.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CaptureController : ControllerBase
+    {
+        private readonly CaptureService _captureService;
+
+        public CaptureController(CaptureService captureService) => _captureService = captureService;
+
+        [HttpPost]
+        public async Task<IActionResult> Post()
+        {
+            return await Task.FromResult(new OkResult());
+        }
+    }
+}
---
+++ src/FasTnT.Host/Controllers/QueryController.cs
@@ -0,0 +1,34 @@
+﻿using FasTnT.Domain.Services;
+using FasTnT.Model.Queries;
+using FasTnT.Model.Subscriptions;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace FasTnT.Host.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class QueryController : ControllerBase
+    {
+        private readonly QueryService _queryService;
+
+        public QueryController(QueryService queryService) => _queryService = queryService;
+
+        [HttpGet]
+        [Route("queries")]
+        public async Task<object> Queries() =>  await _queryService.Process(new GetQueryNames());
+
+        // TODO: get query parameters from OData query string
+        [HttpGet]
+        [Route("queries/{queryName}/events")]
+        public async Task<object> Poll(string queryName) => await _queryService.Process(new Poll { QueryName = queryName, Parameters = new QueryParameter[0] });
+
+        [HttpGet]
+        [Route("queries/{queryName}/subscriptions")]
+        public async Task Unsubscribe(string queryName) => await _queryService.Process(new GetSubscriptionIds { QueryName = queryName });
+
+        [HttpDelete]
+        [Route("queries/{queryName}/subscriptions/{subscriptionId}")]
+        public async Task Unsubscribe(string queryName, string subscriptionId) => _queryService.Process(new UnsubscribeRequest { SubscriptionId = subscriptionId });
+    }
+}
