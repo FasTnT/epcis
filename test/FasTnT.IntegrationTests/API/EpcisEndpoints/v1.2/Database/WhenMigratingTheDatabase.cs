ba2a4c9f95e743591362e0a97627bfbb9bab94d1
Louis-Axel Ambroise <ambroise.la@gmail.com>
2019-05-02 12:31:47 +0200

    Prepare integration tests

---
+++ test/FasTnT.IntegrationTests/API/EpcisEndpoints/v1.2/Database/WhenMigratingTheDatabase.cs
@@ -0,0 +1,21 @@
+﻿using FasTnT.IntegrationTests.Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net;
+
+namespace FasTnT.IntegrationTests.API.EpcisEndpoints.v1_2.Database
+{
+    [TestClass]
+    public class WhenCallingDatabaseEndpointMigrate : BaseIntegrationTest
+    {
+        public override void Act()
+        {
+            Result = Client.PostAsync("/EpcisServices/1.2/Database/Migrate", null).Result;
+        }
+
+        [Assert]
+        public void ItShouldReturnHttp200OK()
+        {
+            Assert.AreEqual(HttpStatusCode.OK, Result.StatusCode);
+        }
+    }
+}
---
+++ test/FasTnT.IntegrationTests/Common/BaseIntegrationTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net.Http;
+
+namespace FasTnT.IntegrationTests.Common
+{
+    public abstract class BaseIntegrationTest
+    {
+        public HttpClient Client { get; set; }
+        public HttpResponseMessage Result { get; set; }
+
+        public virtual void Arrange()
+        {
+            Client = IntegrationTest.Client;
+        }
+
+        public abstract void Act();
+
+        [TestInitialize]
+        public void Execute()
+        {
+            Arrange();
+            Act();
+        }
+    }
+}
