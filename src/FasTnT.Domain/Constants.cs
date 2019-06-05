937160cb0a21689b972c0eed172ddc6a9d111885
Louis-Axel Ambroise <ambroise.la@pm.me>
2019-06-05 15:49:00 +0200

    Set version to 1.0.0

--- src/FasTnT.Domain/Constants.cs
+++ src/FasTnT.Domain/Constants.cs
@@ -3,7 +3,7 @@
     public static class Constants
     {
         public static string StandardVersion = "1.2";
-        public static string ProductVersion = "0.6.0";
+        public static string ProductVersion = "1.0.0";
         public static int SubscriptionTaskDelayTimeoutInMs = 5000;
     }
 }
