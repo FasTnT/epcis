fd48ea12e07ca3ddac9660182e8933c5fc806f52
Louis-Axel Ambroise <ambroise.la@gmail.com>
2019-06-17 08:23:32 +0200

    Add formatters unit tests
    Begin Soap and JSON formatters unit tests

--- test/FasTnT.UnitTest/FormattersTests/XmlFormatter/WhenParsingAValidEventRequestWithMasterdata.cs
+++ test/FasTnT.UnitTest/FormattersTests/XmlFormatter/WhenParsingAValidEventRequestWithMasterdata.cs
@@ -21,7 +21,7 @@ namespace FasTnT.UnitTest.XmlFormatter
         public override void Arrange()
         {
             RequestParser = Formatters.Xml.XmlFormatter.Instance;
-            InputFile = File.OpenRead("XmlFormatter/files/requests/xml/valid_event_masterdata_1.xml");
+            InputFile = File.OpenRead("FormattersTests/XmlFormatter/files/requests/xml/valid_event_masterdata_1.xml");
         }
 
         public override void Act()
