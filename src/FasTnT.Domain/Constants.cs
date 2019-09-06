f1c343a9af91c3d105dbae410df28b4cdcbd4b9b
Louis-Axel Ambroise <ambroise.la@pm.me>
2019-09-06 16:16:11 +0200

    Update product version to 1.2.0

--- src/FasTnT.Domain/Constants.cs
+++ src/FasTnT.Domain/Constants.cs
@@ -3,7 +3,7 @@ namespace FasTnT.Domain
     public static class Constants
     {
         public static string StandardVersion = "1.2";
-        public static string ProductVersion = "1.1.0";
+        public static string ProductVersion = "1.2.0";
         public static int SubscriptionTaskDelayTimeoutInMs = 5000;
     }
 }
