9d991fa68e3afd30707b88fc2318bcaa611c5417
Louis-Axel Ambroise <ambroise.la@gmail.com>
2019-09-20 17:08:03 +0200

    Store header custom fields

--- src/FasTnT.Persistence.Dapper/PgSqlRequestManager.cs
+++ src/FasTnT.Persistence.Dapper/PgSqlRequestManager.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using FasTnT.Domain.Persistence;
@@ -19,6 +21,7 @@ namespace FasTnT.Persistence.Dapper
             var epcisRequest = ModelMapper.Map<EpcisRequestHeader, RequestHeaderEntity>(request, r => { r.UserId = user?.Id; });
             epcisRequest.Id = await _unitOfWork.Store(PgSqlRequestRequests.Store, epcisRequest, cancellationToken);
             await StoreStandardBusinessHeader(request, epcisRequest, cancellationToken);
+            await StoreCustomFields(request, epcisRequest, cancellationToken);
 
             return epcisRequest.Id;
         }
@@ -32,6 +35,27 @@ namespace FasTnT.Persistence.Dapper
 
             await _unitOfWork.Execute(PgSqlRequestRequests.StoreStandardHeader, header, cancellationToken);
             await _unitOfWork.BulkExecute(PgSqlRequestRequests.StoreStandardHeaderContactInformations, contactInformations, cancellationToken);
+        }
+
+        private async Task StoreCustomFields(EpcisRequestHeader request, RequestHeaderEntity epcisRequest, CancellationToken cancellationToken)
+        {
+            var fields = new List<CustomFieldEntity>();
+            ParseFields(request.CustomFields, epcisRequest.Id, fields);
+
+            await _unitOfWork.BulkExecute(PgSqlRequestRequests.StoreCustomFields, fields, cancellationToken);
+        }
+
+        private static void ParseFields(IList<CustomField> customFields, int eventId, List<CustomFieldEntity> mappedList, int? parentId = null)
+        {
+            if (customFields == null || !customFields.Any()) return;
+
+            foreach (var field in customFields)
+            {
+                var entity = field.Map<CustomField, CustomFieldEntity>(f => { f.EventId = eventId; f.Id = mappedList.Count; f.ParentId = parentId; });
+                mappedList.Add(entity);
+
+                ParseFields(field.Children, eventId, mappedList, entity.Id);
+            }
         }
     }
 }
