1c536048e1a81dafb0c9c6b73dc3a29eb528c83a
Louis-Axel Ambroise <ambroise.la@gmail.com>
2019-10-03 16:30:10 +0200

    Fix event formatting
    - Update BaseExtension element (add eventID)
    - Fix EPC types order (outputQuantityList should be last item to appear in the list.

--- src/FasTnT.Formatters.Xml/Formatters/XmlEventFormatter.cs
+++ src/FasTnT.Formatters.Xml/Formatters/XmlEventFormatter.cs
@@ -46,17 +46,27 @@ namespace FasTnT.Formatters.Xml.Responses
             element.Add(new XElement("eventTime", @event.EventTime.ToString(DateTimeFormat, CultureInfo.InvariantCulture)));
             element.Add(new XElement("recordTime", @event.CaptureTime.ToString(DateTimeFormat)));
             element.Add(new XElement("eventTimeZoneOffset", @event.EventTimeZoneOffset.Representation));
-            if (@event.ErrorDeclaration != null) AddErrorDeclaration(@event.ErrorDeclaration, element);
-            if (!string.IsNullOrEmpty(@event.EventId)) element.Add(new XElement("eventID", @event.EventId));
+            if (@event.ErrorDeclaration != null || !string.IsNullOrEmpty(@event.EventId)) AddErrorDeclaration(@event, element);
 
             return element;
         }
 
-        private void AddErrorDeclaration(ErrorDeclaration eventError, XElement element)
+        private void AddErrorDeclaration(EpcisEvent @event, XElement element)
         {
-            var correctiveEventIds = eventError.CorrectiveEventIds.Any() ? new XElement("correctiveEventIDs", eventError.CorrectiveEventIds.Select(x => new XElement("correctiveEventId", x.CorrectiveId))) : null;
-            var errorDeclaration = new XElement("errorDeclaration", new XElement("declarationTime", eventError.DeclarationTime), new XElement("reason", eventError.Reason), correctiveEventIds);
-            element.Add(new XElement("baseExtension", errorDeclaration));
+            XElement errorDeclaration = default, eventId = default;
+            if (@event.ErrorDeclaration != null)
+            {
+                var correctiveEventIds = @event.ErrorDeclaration.CorrectiveEventIds.Any() ? new XElement("correctiveEventIDs", @event.ErrorDeclaration.CorrectiveEventIds.Select(x => new XElement("correctiveEventID", x.CorrectiveId))) : null;
+                errorDeclaration = new XElement("errorDeclaration", new XElement("declarationTime", @event.ErrorDeclaration.DeclarationTime), new XElement("reason", @event.ErrorDeclaration.Reason), correctiveEventIds);
+            }
+
+            if (!string.IsNullOrEmpty(@event.EventId))
+            {
+                eventId = new XElement("eventID", @event.EventId);
+            }
+
+
+            element.Add(new XElement("baseExtension", eventId, errorDeclaration));
         }
 
         public void EpcListMandatory(EpcisEvent evt, XContainer element) => EpcList(evt, element, true);
@@ -75,8 +85,8 @@ namespace FasTnT.Formatters.Xml.Responses
             if (inputEpcList.HasElements) element.Add(inputEpcList);
             if (inputQuantity.HasElements) element.Add(inputQuantity);
             if (quantityList.HasElements) AddInExtension(element, quantityList);
-            if (outputQuantity.HasElements) element.Add(outputQuantity);
             if (outputEpcList.HasElements) element.Add(outputEpcList);
+            if (outputQuantity.HasElements) element.Add(outputQuantity);
         }
 
         public void QuantityEpc(EpcisEvent evt, XContainer element)
