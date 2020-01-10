162f66944d9a1fa0cc94c74cc1a1c0d5073a3f29
Louis-Axel Ambroise <ambroise.la@gmail.com>
2020-01-10 08:14:42 +0100

    Update tests

---
+++ test/FasTnT.IntegrationTests/API/EpcisEndpoints/v1.2/XML/Invalid/WhenCallingTheAPIUsingWrongCredentials.cs
@@ -0,0 +1,24 @@
+﻿using FasTnT.IntegrationTests.Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+
+namespace FasTnT.IntegrationTests.API.EpcisEndpoints.v1._2.XML.Invalid
+{
+    [TestClass]
+    [TestCategory("IntegrationTests")]
+    public class WhenCallingTheAPIUsingWrongCredentials : BaseMigratedIntegrationTest
+    {
+        public override void Act()
+        {
+            Client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", "YWRtaW46SW52YWxpZFB3ZA==");
+            Result = Client.PostAsync("/v1_2/Query", new StringContent(File.ReadAllText("Requests/XML/Poll.xml"), Encoding.UTF8, "application/xml")).Result;
+        }
+
+        // TODO: wait for authentication to be implemented.
+        [Assert, Ignore]
+        public void ItShouldReturnHttp401Unauthorized() => Assert.AreEqual(HttpStatusCode.Unauthorized, Result.StatusCode);
+    }
+}
