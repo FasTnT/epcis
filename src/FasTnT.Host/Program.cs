75db11520dfe4c7fe158fa59d8ffcb4343715f73
Louis-Axel Ambroise <ambroise.la@gmail.com>
2020-02-25 12:01:00 +0100

    Add command lines parameters

--- src/FasTnT.Host/Program.cs
+++ src/FasTnT.Host/Program.cs
@@ -1,16 +1,25 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using System;
 
 namespace FasTnT.Host
 {
     public class Program
     {
-        public static void Main(string[] args) => BuildWebHost(args).Run();
-        public static IWebHost BuildWebHost(string[] args) => 
+        public static void Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+            .AddCommandLine(args)
+            .Build();
+
+            BuildWebHost(args, configuration).Run();
+        }
+        public static IWebHost BuildWebHost(string[] args, IConfiguration configuration) => 
             WebHost.CreateDefaultBuilder(args)
             .UseShutdownTimeout(TimeSpan.FromSeconds(10))
             .UseKestrel(c => c.AddServerHeader = false)
+            .UseConfiguration(configuration)
             .UseStartup<Startup>()
             .Build();
     }
