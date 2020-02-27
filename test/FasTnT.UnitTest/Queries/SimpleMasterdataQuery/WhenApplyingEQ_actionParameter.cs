447cb2d34802e90aeb4fdf405a503096695c3831
Louis-Axel Ambroise <ambroise.la@pm.me>
2020-02-27 09:00:51 +0100

    Add command lines parameters (#159)
    * Add command lines parameters
    
    * Add unit tests

---
+++ test/FasTnT.UnitTest/Queries/SimpleMasterdataQuery/WhenApplyingEQ_actionParameter.cs
@@ -0,0 +1,24 @@
+﻿using FasTnT.Domain.Data.Model.Filters;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Linq;
+
+namespace FasTnT.UnitTest.Queries.SimpleMasterdataQuery
+{
+    [TestClass]
+    public class WhenApplyingVocabularyNameParameter : SimpleMasterdataQueryTestBase
+    {
+        public override void Given()
+        {
+            base.Given();
+
+            Parameters.Add(new Model.Queries.QueryParameter { Name = "vocabularyName", Values = new[] { "masterdata-type" } });
+        }
+
+        [TestMethod]
+        public void ItShouldCallTheEventFetcherApplyMethodWithComparisonParameterFilter()
+        {
+            MasterdataFetcher.Verify(x => x.Apply(It.Is<MasterdataTypeFilter>(f => f.Values.Contains("masterdata-type"))), Times.Once);
+        }
+    }
+}
