987d523f0655be6779944c40bcccf604681cadef
Louis-Axel Ambroise <ambroise.la@pm.me>
2020-03-09 15:51:01 +0100

    Implement parameters (#161)
    * Implement parameters
    
    - EQATTR
    - HASATTR
    - "**_"-like parameters
    - "**_INNER_"-like parameters

--- src/FasTnT.Domain/Data/Model/Filters/ExistCustomFieldFilter.cs
+++ src/FasTnT.Domain/Data/Model/Filters/ExistCustomFieldFilter.cs
@@ -1,4 +1,5 @@
 ﻿using FasTnT.Model;
+using FasTnT.Model.Events.Enums;
 
 namespace FasTnT.Domain.Data.Model.Filters
 {
@@ -6,5 +7,18 @@ namespace FasTnT.Domain.Data.Model.Filters
     {
         public CustomField Field { get; set; }
         public bool IsInner { get; set; }
+    }
+
+    public class ExistsAttributeFilter
+    {
+        public EpcisField Field { get; set; }
+        public string AttributeName { get; set; }
+    }
+
+    public class AttributeFilter
+    {
+        public EpcisField Field { get; set; }
+        public string AttributeName { get; set; }
+        public string[] Values { get; set; }
     }
 }
---
+++ test/FasTnT.UnitTest/Queries/SimpleEventQuery/WhenApplyingEQ_Parameter.cs
@@ -0,0 +1,24 @@
+﻿using FasTnT.Domain.Data.Model.Filters;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Linq;
+
+namespace FasTnT.UnitTest.Queries.SimpleEventQuery
+{
+    [TestClass]
+    public class WhenApplyingEQ_Parameter : SimpleEventQueryTestBase
+    {
+        public override void Given()
+        {
+            base.Given();
+
+            Parameters.Add(new Model.Queries.QueryParameter { Name = "EQ_namespace#name", Values = new[] { "1-5" } });
+        }
+
+        [TestMethod]
+        public void ItShouldCallTheEventFetcherApplyMethodWithComparisonCustomFieldParameterFilter()
+        {
+            EventFetcher.Verify(x => x.Apply(It.Is<CustomFieldFilter>(f => f.Field.Namespace == "namespace" && f.Field.Name == "name" && f.IsInner == false && f.Values.Single().ToString() == "1-5")), Times.Once);
+        }
+    }
+}
