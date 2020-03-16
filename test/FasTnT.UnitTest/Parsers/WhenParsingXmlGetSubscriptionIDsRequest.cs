6a618518ffb4bf9e346b6128ae109bffc035e061
Louis-Axel Ambroise <ambroise.la@gmail.com>
2020-03-16 10:04:38 +0100

    Update WhenParsingXmlGetSubscriptionIDsRequest.cs

--- test/FasTnT.UnitTest/Parsers/WhenParsingXmlGetSubscriptionIDsRequest.cs
+++ test/FasTnT.UnitTest/Parsers/WhenParsingXmlGetSubscriptionIDsRequest.cs
@@ -8,13 +8,21 @@ namespace FasTnT.UnitTest.Parsers
     {
         public override void Given()
         {
-            SetRequest("<?xml version=\"1.0\" encoding=\"utf-8\"?><epcisq:EPCISQueryDocument xmlns:epcisq=\"urn:epcglobal:epcis-query:xsd:1\" creationDate=\"2019-01-26T20:10:01.8111457Z\" schemaVersion=\"1\"><EPCISBody><epcisq:GetSubscriptionIDs /></EPCISBody></epcisq:EPCISQueryDocument>");
+            SetRequest("<?xml version=\"1.0\" encoding=\"utf-8\"?><epcisq:EPCISQueryDocument xmlns:epcisq=\"urn:epcglobal:epcis-query:xsd:1\" creationDate=\"2019-01-26T20:10:01.8111457Z\" schemaVersion=\"1\"><EPCISBody><epcisq:GetSubscriptionIDs><queryName>SimpleEventQuery</queryName></epcisq:GetSubscriptionIDs></EPCISBody></epcisq:EPCISQueryDocument>");
         }
 
         [TestMethod]
         public void ItShouldReturnAGetSubscriptionIDsRequest()
         {
             Assert.IsInstanceOfType(Result, typeof(GetSubscriptionIdsRequest));
+        }
+
+        [TestMethod]
+        public void ItShouldContainTheExpectedQueryName()
+        {
+            var getSubscriptionIdsRequest = (GetSubscriptionIdsRequest) Result;
+
+            Assert.AreEqual("SimpleEventQuery", getSubscriptionIdsRequest.QueryName);
         }
     }
 }
