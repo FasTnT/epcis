4e8e6651cdc304ccf353de11e8fd00436d6aa362
Louis-Axel Ambroise <ambroise.la@pm.me>
2020-03-25 12:08:01 +0100

    Add XML/SOAP parsing tests (#168)
    * Add unit tests
    
    * Add XML parsing unit tests
    
    * Update WhenParsingXmlGetSubscriptionIDsRequest.cs
    
    * Add XML parsing unit tests
    
    * Add SOAP parsing unit tests
    
    Co-authored-by: Louis-Axel Ambroise <ambroise.la@gmail.com>

---
+++ test/FasTnT.UnitTest/Parsers/Soap/WhenParsingSoapGetVendorVersionRequest.cs
@@ -0,0 +1,20 @@
+﻿using FasTnT.Commands.Requests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FasTnT.UnitTest.Parsers.Soap
+{
+    [TestClass]
+    public class WhenParsingSoapGetVendorVersionRequest : SoapParserTestBase
+    {
+        public override void Given()
+        {
+            SetRequest("<?xml version=\"1.0\" encoding=\"utf-8\"?><soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:urn=\"urn:epcglobal:epcis-query:xsd:1\"><soapenv:Body><urn:GetVendorVersion /></soapenv:Body></soapenv:Envelope>");
+        }
+
+        [TestMethod]
+        public void ItShouldReturnAGetVendorVersionRequest()
+        {
+            Assert.IsInstanceOfType(Result, typeof(GetVendorVersionRequest));
+        }
+    }
+}
