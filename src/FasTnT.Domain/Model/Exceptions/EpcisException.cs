9c24caeaae6b7b5ab59a1cb84c086956db4c0c76
Louis-Axel Ambroise <ambroise.la@pm.me>
2020-08-15 15:15:17 +0200

    Use C#8 + update switch formatting

--- src/FasTnT.Domain/Model/Exceptions/EpcisException.cs
+++ src/FasTnT.Domain/Model/Exceptions/EpcisException.cs
@@ -4,13 +4,15 @@ namespace FasTnT.Model.Exceptions
 {
     public class EpcisException : Exception
     {
+        public static EpcisException Default = new EpcisException(ExceptionType.ImplementationException, string.Empty, ExceptionSeverity.Error);
+
         public ExceptionType ExceptionType { get; }
         public ExceptionSeverity Severity { get; }
 
         public EpcisException(ExceptionType exceptionType, string message, ExceptionSeverity severity = null) : base(message)
         {
             ExceptionType = exceptionType;
-            Severity = severity;
+            Severity = severity ?? ExceptionSeverity.Error;
         }
     }
 }
