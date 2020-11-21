a16128ef1e795370f8c6c2dd7b390179e232e6f4
Louis-Axel Ambroise <ambroise.la@pm.me>
2020-11-21 17:57:54 +0100

    Use Directory.build.props to manage version

--- src/FasTnT.Domain/Constants.cs
+++ src/FasTnT.Domain/Constants.cs
@@ -4,8 +4,8 @@ namespace FasTnT.Domain
 {
     public static class Constants
     {
-        public const string VendorVersion = "2.1.3";
-        public const string StandardVersion = "1.2";
+        public static string VendorVersion => Assembly.GetName().Version.ToString(3);
+        public static string StandardVersion => "1.2";
 
         public static readonly Assembly Assembly = typeof(Constants).Assembly;
     }
